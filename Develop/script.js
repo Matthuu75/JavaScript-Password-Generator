7978dc8c0974b5a249c194ccc6fb89024eb7c93a
Marcus Matthews <mlmatthews75@gmail.com>
2023-07-22 16:55:10 -0400

    added feedback when entering character length.

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,27 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("Clicked")
-//click generate password - prompt generate password criteria
-//
+  //click generate password - prompt generate password criteria
+  var userChoice = window.prompt("How many characters do you want? (8-128 characaters)");
+  if (userChoice < 8) {
+    window.alert("Number must be > 8 characters")
+  }
+  else if (userChoice > 128) {
+    window.alert("Number must be < 128 characters")
+  }
+  if (userChoice === )
 
+
+  //click generate password - password length (8-128 char.)
+  //lower, upper numeric and/or spec characters
+  //validate the input
+  if (!userChoice) {
+    return;
+  }
+  //password will be generated/returned below
   return "Generate password here";
 }
 
