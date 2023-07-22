e51bfd16bf9a0b2891ed6544bdcecb0fffe4b279
Marcus Matthews <mlmatthews75@gmail.com>
2023-07-22 18:18:26 -0400

    added varible choices for each of the criteria asking the users.

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,73 @@
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+var passwordChar = ""
+
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numberCharacters = "0123456789"
+var specialCharacters = "!@#$%^&*()/*-+=[]{};:<>?"
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   console.log("Clicked")
   //click generate password - prompt generate password criteria
-  var userChoice = window.prompt("How many characters do you want? (8-128 characaters)");
-  if (userChoice < 8) {
-    window.alert("Number must be > 8 characters")
-  }
-  else if (userChoice > 128) {
-    window.alert("Number must be < 128 characters")
-  }
-  if (userChoice === )
+  var userChoiceLength = window.prompt("How many characters do you want? Choose between 8-128");
 
-
-  //click generate password - password length (8-128 char.)
-  //lower, upper numeric and/or spec characters
-  //validate the input
-  if (!userChoice) {
+  if (userChoiceLength < 8 || userChoiceLength > 128) {
+    window.alert("Number must be > 8 characters & < 128 characters")
     return;
   }
-  //password will be generated/returned below
-  return "Generate password here";
+  else if (userChoiceLength > 8 || userChoiceLength < 128) {
+    window.alert("You got it!")
+  }
+
+  var userChoiceLowercase = window.confirm("Want some lowercase letters?")
+  if (userChoiceLowercase) {
+    passwordChar += lowercaseCharacters
+    console.log("adding lowercase letters")
+  }
+  else if (!userChoiceLowercase) {
+    console.log("no lowercase letters")
+  }
+
+  var userChoiceUppercase = window.confirm("Want any uppercase letters?")
+  if (userChoiceUppercase) {
+    passwordChar += uppercaseCharacters
+    console.log("adding uppercase letters")
+  }
+  else if (!userChoiceUppercase) {
+    console.log("no uppercase letters")
+  }
+
+  var userChoiceNumbers = window.confirm("Want any numbers?")
+  if (userChoiceNumbers) {
+    passwordChar += numberCharacters
+    console.log("adding numbers")
+  }
+  else if (!userChoiceNumbers) {
+    console.log("no numbers")
+  }
+
+  var userChoiceSpecCharacters = window.confirm("Want some special characters?")
+  if (userChoiceSpecCharacters) {
+    passwordChar += specialCharacters
+    console.log("adding special characters")
+  }
+  else if (!userChoiceSpecCharacters) {
+    console.log("no special characters")
+  }
+
+  window.alert("Ready to see your password?")
+
+  return;
 }
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
